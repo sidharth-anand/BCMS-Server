895dd4c8ed57bce4df872bb7103d3d48b6776027
Sidharth Anand <sidharthanand85@gmail.com>
2021-03-29 12:16:27 +0530

    Added init processing for course and post

--- db/init.js
+++ db/init.js
@@ -4,6 +4,8 @@ async function init() {
     db.processFile("./db/queries/create/common.sql");
 
     db.processFile("./db/queries/create/user.sql");
+    db.processFile("./db/queries/create/course.sql");
+    db.processFile("./db/queries/create/post.sql");
 }
 
 module.exports = {
