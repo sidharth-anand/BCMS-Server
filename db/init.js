6e8621937dcb6bf1b3f24c670a92d47d70701d32
Aryan Chaubal <f20190130@hyderabad.bits-pilani.ac.in>
2021-03-29 13:52:39 +0530

    Fixed errors in tables

--- db/init.js
+++ db/init.js
@@ -6,6 +6,13 @@ async function init() {
     db.processFile("./db/queries/create/user.sql");
     db.processFile("./db/queries/create/course.sql");
     db.processFile("./db/queries/create/post.sql");
+    db.processFile("./db/queries/create/role.sql");
+    db.processFile("./db/queries/create/tag.sql");
+
+    db.processFile("./db/queries/create/notification.sql");
+    db.processFile("./db/queries/create/registered_in.sql");
+    db.processFile("./db/queries/create/post_tag.sql");
+    db.processFile("./db/queries/create/user_role.sql");
 }
 
 module.exports = {
