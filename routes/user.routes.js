f68f9f70b76809a450896955d8ea3424fc75f994
aryanarora180 <aryan.arora180@gmail.com>
2021-04-09 14:49:50 +0530

    Removed extra create user route

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -34,48 +34,4 @@ router.get("user/:id/courses", authService.validate(),  (req, res, next) => {
     });
 });
 
-router.post("user/create", authService.validate(), (async (req, res) => {
-    const username = req.body.user_name
-    const displayName = req.body.display_name
-    const bio = req.body.bio || ""
-    const email = req.body.email
-    const phone = req.body.phone
-    const password = req.body.password
-
-    const validationResult = await validateUserDetails(username, displayName, bio, email, phone, password)
-    if (validationResult.is_valid) {
-        await authService.register({
-            // todo: add user details over here
-        }, (err, queryRes) => {
-            if (!err) {
-                res.send(queryRes.rows);
-            } else {
-                res.status(500).send({name: err.name, message: err.message})
-            }
-        })
-    } else {
-        res.status(400).send(validationResult.error)
-    }
-}))
-
-async function validateUserDetails(username, displayName, bio, email, phone, password) {
-    if (await authService.isUsernameNotUnique(username)) {
-        return {
-            is_valid: false,
-            error: {
-                name: "Username is already taken",
-                message: "The username selected by you has already been taken by someone else"
-            }
-        }
-    }
-
-    if (displayName == null && typeof displayName === 'string') {
-
-    }
-
-    // todo: validate other details
-
-    return true
-}
-
 module.exports = router;
