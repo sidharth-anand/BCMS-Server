c8b83fbc7c0c29a53e554ec67c7cc0a310290e4a
Sidharth Anand <sidharthanand85@gmail.com>
2021-04-19 14:38:58 +0530

    Db init now only runs on dev startup

--- db/init.js
+++ db/init.js
@@ -1,21 +1,22 @@
 const db = require('./db');
 
 async function init() {
-    db.processFileSequence([
-        "./db/queries/create/common.sql",
-
-        "./db/queries/create/user.sql",
-        "./db/queries/create/course.sql",
-        "./db/queries/create/post.sql",
-        "./db/queries/create/role.sql",
-        "./db/queries/create/tag.sql",
-        "./db/queries/create/notification.sql",
-
-        "./db/queries/create/registered_in.sql",
-        "./db/queries/create/post_tag.sql",
-        "./db/queries/create/user_role.sql"
-    ]);
-
+    if(process.env.NODE_ENV === 'dev') {
+        db.processFileSequence([
+            "./db/queries/create/common.sql",
+    
+            "./db/queries/create/user.sql",
+            "./db/queries/create/course.sql",
+            "./db/queries/create/post.sql",
+            "./db/queries/create/role.sql",
+            "./db/queries/create/tag.sql",
+            "./db/queries/create/notification.sql",
+    
+            "./db/queries/create/registered_in.sql",
+            "./db/queries/create/post_tag.sql",
+            "./db/queries/create/user_role.sql"
+        ]);
+    }
 }
 
 module.exports = {
